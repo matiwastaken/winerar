51b33110e74b0064075adef9df3bcf80b2320c9e
mateusz <james.hetfield.official.yeah@gmail.com>
2025-03-08 06:31:19 -0800

    Winerar portable

---
+++ main.bat
@@ -0,0 +1,105 @@
+@echo off
+title WineRar Patcher ^| v0.1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Set aisq=R07DnxFZkW1G2Yms48VdljOuNKwhAXBCSqcJT5yEMe93PzQHLaporIUf6vbtig
+cls
+@%aisq:~41,1%%aisq:~34,1%%aisq:~27,1%%aisq:~51,1% %aisq:~51,1%%aisq:~55,1%%aisq:~55,1%
+
+%aisq:~41,1%%aisq:~34,1%%aisq:~27,1%%aisq:~51,1% %aisq:~9,1%%aisq:~60,1%%aisq:~4,1%%aisq:~41,1%%aisq:~0,1%%aisq:~49,1%%aisq:~52,1% %aisq:~44,1%%aisq:~49,1%%aisq:~59,1%%aisq:~34,1%%aisq:~27,1%%aisq:~41,1%%aisq:~52,1% ^| %aisq:~61,1%%aisq:~60,1%%aisq:~59,1%%aisq:~27,1%%aisq:~23,1%%aisq:~58,1%.%aisq:~34,1%%aisq:~51,1%%aisq:~14,1%/%aisq:~14,1%%aisq:~49,1%%aisq:~59,1%%aisq:~60,1%%aisq:~26,1%%aisq:~49,1%%aisq:~15,1%%aisq:~59,1%%aisq:~49,1%%aisq:~8,1%%aisq:~41,1%%aisq:~4,1%/%aisq:~26,1%%aisq:~60,1%%aisq:~4,1%%aisq:~41,1%%aisq:~52,1%%aisq:~49,1%%aisq:~52,1% ^| %aisq:~14,1%%aisq:~49,1%%aisq:~19,1%%aisq:~41,1% %aisq:~58,1%%aisq:~38,1% %aisq:~14,1%%aisq:~49,1%%aisq:~59,1%%aisq:~41,1%%aisq:~23,1%%aisq:~15,1%%aisq:~45,1%
+%aisq:~59,1%%aisq:~60,1%%aisq:~14,1%%aisq:~41,1%%aisq:~51,1%%aisq:~23,1%%aisq:~59,1% /%aisq:~59,1% %aisq:~1,1% >%aisq:~4,1%%aisq:~23,1%%aisq:~20,1%
+%aisq:~41,1%%aisq:~34,1%%aisq:~27,1%%aisq:~51,1% %aisq:~44,1%%aisq:~52,1%%aisq:~41,1%%aisq:~15,1%%aisq:~15,1% %aisq:~28,1%%aisq:~4,1%%aisq:~38,1% %aisq:~8,1%%aisq:~41,1%%aisq:~38,1% %aisq:~59,1%%aisq:~51,1% %aisq:~50,1%%aisq:~49,1%%aisq:~59,1%%aisq:~34,1%%aisq:~27,1% %aisq:~9,1%%aisq:~60,1%%aisq:~4,1%%aisq:~52,1%%aisq:~49,1%%aisq:~52,1%
+%aisq:~50,1%%aisq:~49,1%%aisq:~23,1%%aisq:~15,1%%aisq:~41,1% >%aisq:~4,1%%aisq:~23,1%%aisq:~20,1%
+
+%aisq:~59,1%%aisq:~60,1%%aisq:~14,1%%aisq:~41,1%%aisq:~51,1%%aisq:~23,1%%aisq:~59,1% /%aisq:~59,1% %aisq:~10,1% >%aisq:~4,1%%aisq:~23,1%%aisq:~20,1%
+(
+%aisq:~41,1%%aisq:~34,1%%aisq:~27,1%%aisq:~51,1% %aisq:~0,1%%aisq:~28,1%%aisq:~0,1% %aisq:~52,1%%aisq:~41,1%%aisq:~61,1%%aisq:~60,1%%aisq:~15,1%%aisq:~59,1%%aisq:~52,1%%aisq:~49,1%%aisq:~59,1%%aisq:~60,1%%aisq:~51,1%%aisq:~4,1% %aisq:~19,1%%aisq:~49,1%%aisq:~59,1%%aisq:~49,1%
+%aisq:~41,1%%aisq:~34,1%%aisq:~27,1%%aisq:~51,1% %aisq:~9,1%%aisq:~60,1%%aisq:~4,1%%aisq:~0,1%%aisq:~28,1%%aisq:~0,1%
+%aisq:~41,1%%aisq:~34,1%%aisq:~27,1%%aisq:~51,1% %aisq:~54,1%%aisq:~4,1%%aisq:~20,1%%aisq:~60,1%%aisq:~14,1%%aisq:~60,1%%aisq:~59,1%%aisq:~41,1%%aisq:~19,1% %aisq:~31,1%%aisq:~51,1%%aisq:~14,1%%aisq:~50,1%%aisq:~49,1%%aisq:~4,1%%aisq:~38,1% %aisq:~48,1%%aisq:~60,1%%aisq:~34,1%%aisq:~41,1%%aisq:~4,1%%aisq:~15,1%%aisq:~41,1%
+%aisq:~41,1%%aisq:~34,1%%aisq:~27,1%%aisq:~51,1% %aisq:~54,1%%aisq:~53,1%%aisq:~3,1%=%aisq:~16,1%%aisq:~58,1%%aisq:~42,1%%aisq:~10,1%%aisq:~16,1%%aisq:~55,1%%aisq:~58,1%%aisq:~2,1%%aisq:~2,1%%aisq:~12,1%%aisq:~34,1%%aisq:~17,1%%aisq:~43,1%%aisq:~2,1%%aisq:~56,1%%aisq:~58,1%%aisq:~55,1%%aisq:~37,1%%aisq:~2,1%%aisq:~10,1%
+%aisq:~41,1%%aisq:~34,1%%aisq:~27,1%%aisq:~51,1% %aisq:~56,1%%aisq:~16,1%%aisq:~10,1%%aisq:~12,1%%aisq:~12,1%%aisq:~10,1%%aisq:~12,1%%aisq:~12,1%%aisq:~37,1%%aisq:~1,1%%aisq:~55,1%%aisq:~37,1%%aisq:~2,1%%aisq:~10,1%%aisq:~10,1%%aisq:~49,1%%aisq:~19,1%%aisq:~1,1%%aisq:~2,1%%aisq:~12,1%%aisq:~34,1%%aisq:~55,1%%aisq:~43,1%%aisq:~37,1%%aisq:~10,1%%aisq:~34,1%%aisq:~55,1%%aisq:~49,1%%aisq:~43,1%%aisq:~42,1%%aisq:~41,1%%aisq:~12,1%%aisq:~17,1%%aisq:~37,1%%aisq:~10,1%%aisq:~10,1%%aisq:~42,1%%aisq:~12,1%%aisq:~19,1%%aisq:~49,1%%aisq:~55,1%%aisq:~17,1%%aisq:~49,1%%aisq:~43,1%%aisq:~56,1%%aisq:~12,1%%aisq:~56,1%%aisq:~17,1%%aisq:~10,1%%aisq:~58,1%%aisq:~58,1%%aisq:~58,1%%aisq:~10,1%%aisq:~19,1%
+%aisq:~41,1%%aisq:~34,1%%aisq:~27,1%%aisq:~51,1% %aisq:~34,1%%aisq:~19,1%%aisq:~16,1%%aisq:~17,1%%aisq:~19,1%%aisq:~49,1%%aisq:~10,1%%aisq:~19,1%%aisq:~10,1%%aisq:~16,1%%aisq:~19,1%%aisq:~42,1%%aisq:~42,1%%aisq:~37,1%%aisq:~55,1%%aisq:~1,1%%aisq:~58,1%%aisq:~58,1%%aisq:~55,1%%aisq:~42,1%%aisq:~56,1%%aisq:~1,1%%aisq:~55,1%%aisq:~34,1%%aisq:~41,1%%aisq:~56,1%%aisq:~34,1%%aisq:~58,1%%aisq:~37,1%%aisq:~55,1%%aisq:~55,1%%aisq:~19,1%%aisq:~41,1%%aisq:~56,1%%aisq:~12,1%%aisq:~17,1%%aisq:~42,1%%aisq:~1,1%%aisq:~1,1%%aisq:~2,1%%aisq:~42,1%%aisq:~17,1%%aisq:~56,1%%aisq:~10,1%%aisq:~58,1%%aisq:~41,1%%aisq:~37,1%%aisq:~2,1%%aisq:~56,1%%aisq:~43,1%%aisq:~17,1%%aisq:~2,1%%aisq:~10,1%%aisq:~2,1%
+%aisq:~41,1%%aisq:~34,1%%aisq:~27,1%%aisq:~51,1% %aisq:~2,1%%aisq:~10,1%%aisq:~43,1%%aisq:~10,1%%aisq:~34,1%%aisq:~41,1%%aisq:~19,1%%aisq:~17,1%%aisq:~43,1%%aisq:~37,1%%aisq:~41,1%%aisq:~19,1%%aisq:~56,1%%aisq:~37,1%%aisq:~34,1%%aisq:~34,1%%aisq:~2,1%%aisq:~16,1%%aisq:~43,1%%aisq:~19,1%%aisq:~42,1%%aisq:~2,1%%aisq:~2,1%%aisq:~2,1%%aisq:~55,1%%aisq:~12,1%%aisq:~41,1%%aisq:~49,1%%aisq:~2,1%%aisq:~10,1%%aisq:~49,1%%aisq:~17,1%%aisq:~41,1%%aisq:~43,1%%aisq:~12,1%%aisq:~34,1%%aisq:~2,1%%aisq:~41,1%%aisq:~37,1%%aisq:~42,1%%aisq:~43,1%%aisq:~34,1%%aisq:~55,1%%aisq:~56,1%%aisq:~56,1%%aisq:~2,1%%aisq:~42,1%%aisq:~16,1%%aisq:~43,1%%aisq:~16,1%%aisq:~43,1%%aisq:~37,1%%aisq:~56,1%%aisq:~37,1%
+%aisq:~41,1%%aisq:~34,1%%aisq:~27,1%%aisq:~51,1% %aisq:~58,1%%aisq:~16,1%%aisq:~10,1%%aisq:~58,1%%aisq:~34,1%%aisq:~55,1%%aisq:~37,1%%aisq:~56,1%%aisq:~42,1%%aisq:~12,1%%aisq:~42,1%%aisq:~16,1%%aisq:~17,1%%aisq:~56,1%%aisq:~58,1%%aisq:~17,1%%aisq:~58,1%%aisq:~34,1%%aisq:~19,1%%aisq:~49,1%%aisq:~34,1%%aisq:~43,1%%aisq:~43,1%%aisq:~19,1%%aisq:~37,1%%aisq:~1,1%%aisq:~41,1%%aisq:~34,1%%aisq:~55,1%%aisq:~2,1%%aisq:~2,1%%aisq:~43,1%%aisq:~42,1%%aisq:~42,1%%aisq:~56,1%%aisq:~1,1%%aisq:~37,1%%aisq:~12,1%%aisq:~37,1%%aisq:~42,1%%aisq:~17,1%%aisq:~55,1%%aisq:~10,1%%aisq:~55,1%%aisq:~37,1%%aisq:~37,1%%aisq:~56,1%%aisq:~19,1%%aisq:~41,1%%aisq:~55,1%%aisq:~55,1%%aisq:~55,1%%aisq:~58,1%%aisq:~19,1%
+%aisq:~41,1%%aisq:~34,1%%aisq:~27,1%%aisq:~51,1% %aisq:~42,1%%aisq:~17,1%%aisq:~12,1%%aisq:~55,1%%aisq:~58,1%%aisq:~41,1%%aisq:~2,1%%aisq:~10,1%%aisq:~41,1%%aisq:~42,1%%aisq:~43,1%%aisq:~19,1%%aisq:~55,1%%aisq:~56,1%%aisq:~58,1%%aisq:~56,1%%aisq:~43,1%%aisq:~16,1%%aisq:~56,1%%aisq:~34,1%%aisq:~43,1%%aisq:~2,1%%aisq:~49,1%%aisq:~43,1%%aisq:~17,1%%aisq:~42,1%%aisq:~1,1%%aisq:~55,1%%aisq:~43,1%%aisq:~34,1%%aisq:~2,1%%aisq:~41,1%%aisq:~19,1%%aisq:~34,1%%aisq:~56,1%%aisq:~37,1%%aisq:~19,1%%aisq:~2,1%%aisq:~55,1%%aisq:~37,1%%aisq:~16,1%%aisq:~37,1%%aisq:~37,1%%aisq:~16,1%%aisq:~2,1%%aisq:~1,1%%aisq:~19,1%%aisq:~10,1%%aisq:~43,1%%aisq:~19,1%%aisq:~10,1%%aisq:~10,1%%aisq:~42,1%%aisq:~1,1%
+%aisq:~41,1%%aisq:~34,1%%aisq:~27,1%%aisq:~51,1% %aisq:~56,1%%aisq:~41,1%%aisq:~56,1%%aisq:~55,1%%aisq:~58,1%%aisq:~17,1%%aisq:~12,1%%aisq:~16,1%%aisq:~58,1%%aisq:~34,1%%aisq:~55,1%%aisq:~12,1%%aisq:~37,1%%aisq:~55,1%%aisq:~10,1%%aisq:~37,1%%aisq:~37,1%%aisq:~37,1%%aisq:~16,1%%aisq:~2,1%%aisq:~58,1%%aisq:~37,1%%aisq:~55,1%%aisq:~34,1%%aisq:~16,1%%aisq:~10,1%%aisq:~42,1%%aisq:~1,1%%aisq:~10,1%%aisq:~49,1%%aisq:~19,1%%aisq:~37,1%%aisq:~17,1%%aisq:~34,1%%aisq:~1,1%%aisq:~42,1%%aisq:~42,1%%aisq:~12,1%%aisq:~55,1%%aisq:~37,1%%aisq:~2,1%%aisq:~1,1%%aisq:~58,1%%aisq:~41,1%%aisq:~10,1%%aisq:~34,1%%aisq:~55,1%%aisq:~37,1%%aisq:~56,1%%aisq:~1,1%%aisq:~17,1%%aisq:~58,1%%aisq:~49,1%%aisq:~42,1%
+%aisq:~41,1%%aisq:~34,1%%aisq:~27,1%%aisq:~51,1% %aisq:~49,1%%aisq:~41,1%%aisq:~55,1%%aisq:~56,1%%aisq:~42,1%%aisq:~19,1%%aisq:~16,1%%aisq:~17,1%%aisq:~34,1%%aisq:~17,1%%aisq:~56,1%%aisq:~16,1%%aisq:~58,1%%aisq:~34,1%%aisq:~19,1%%aisq:~2,1%%aisq:~12,1%%aisq:~19,1%%aisq:~10,1%%aisq:~37,1%%aisq:~10,1%%aisq:~56,1%%aisq:~43,1%%aisq:~17,1%%aisq:~42,1%%aisq:~2,1%%aisq:~2,1%%aisq:~2,1%%aisq:~43,1%%aisq:~19,1%%aisq:~43,1%%aisq:~10,1%%aisq:~16,1%%aisq:~10,1%%aisq:~17,1%%aisq:~2,1%%aisq:~55,1%%aisq:~56,1%%aisq:~49,1%%aisq:~42,1%%aisq:~49,1%%aisq:~55,1%%aisq:~43,1%%aisq:~37,1%%aisq:~1,1%%aisq:~17,1%%aisq:~1,1%%aisq:~17,1%%aisq:~2,1%%aisq:~10,1%%aisq:~42,1%%aisq:~2,1%%aisq:~42,1%%aisq:~56,1%
+) > %aisq:~52,1%%aisq:~49,1%%aisq:~52,1%%aisq:~52,1%%aisq:~41,1%%aisq:~61,1%.%aisq:~8,1%%aisq:~41,1%%aisq:~38,1%
